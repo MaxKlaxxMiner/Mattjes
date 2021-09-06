635f55841520e7cf0b255c43329cdc78cfd42da1
MaxKlaxxMiner <max@tetrisk.de>
2021-09-06 21:20:19 +0200

    2x rook mates

--- Tests/Tests.MateReverse.cs
+++ Tests/Tests.MateReverse.cs
@@ -12,33 +12,35 @@ namespace Mattjes
   public static partial class Tests
   {
     static int counter;
+    static readonly HashSet<ulong> mateHashes = new HashSet<ulong>();
 
-    static void MateReverseInternal(IBoard b, Piece[] pieces, int piecesIndex)
+    static void MateReverseInternal(IBoard b, Piece[] pieces, int piecesIndex, Func<IBoard, bool> validPosition)
     {
       Debug.Assert(pieces.Length > 0);
       Debug.Assert(piecesIndex < pieces.Length);
-      piecesIndex++;
 
-      if (piecesIndex < pieces.Length)
-      {
-        throw new NotImplementedException();
-      }
-
-      var piece = pieces[pieces.Length - 1];
+      var piece = pieces[piecesIndex++];
 
       for (int pos = 0; pos < IBoard.Width * IBoard.Height; pos++)
       {
         if (b.GetField(pos) != Piece.None) continue; // Feld schon besetzt?
 
         b.SetField(pos, piece);
-        if (BoardTools.IsMate(b))
+        if (BoardTools.IsMate(b) && validPosition(b))
         {
-          Console.WriteLine();
-          BoardTools.PrintBoard(b);
-          Console.WriteLine("    Counter: {0:N0}", ++counter);
+          counter++;
+          ulong cs = b.GetChecksum();
+          if (!mateHashes.Add(cs)) continue;
+          //Console.WriteLine();
+          //BoardTools.PrintBoard(b);
+          //Console.WriteLine();
           //Console.ReadLine();
-
         }
+        if (piecesIndex < pieces.Length)
+        {
+          MateReverseInternal(b, pieces, piecesIndex, validPosition);
+        }
+
         b.SetField(pos, Piece.None);
       }
     }
@@ -48,7 +50,11 @@ namespace Mattjes
       //IBoard b = new BoardReference();
       IBoard b = new BoardKingOptimized3();
 
-      var pieces = new[] { Piece.WhiteQueen };
+      //var pieces = new[] { Piece.WhiteQueen };
+      //var pieces = new[] { Piece.WhiteRook };
+      var pieces = new[] { Piece.WhiteRook, Piece.WhiteRook };
+      //var pieces = new[] { Piece.WhiteBishop, Piece.WhiteBishop };
+      Func<IBoard, bool> validPosition = board => true;
 
       b.Clear();
       b.WhiteMove = false;
@@ -58,12 +64,17 @@ namespace Mattjes
         b.SetField(kingPairs.Key, Piece.BlackKing);
         b.SetField(kingPairs.Value, Piece.WhiteKing);
 
-        MateReverseInternal(b, pieces, 0);
+        MateReverseInternal(b, pieces, 0, validPosition);
 
         b.SetField(kingPairs.Key, Piece.None);
         b.SetField(kingPairs.Value, Piece.None);
       }
 
+      Console.WriteLine();
+      Console.WriteLine("    Mate-Counter: {0:N0}", counter);
+      Console.WriteLine();
+      Console.WriteLine("    Unique-Mates: {0:N0}", mateHashes.Count);
+      Console.WriteLine();
     }
   }
 }
